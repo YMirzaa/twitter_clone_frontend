201a5a7e227eab2ec8f41ab6e69f689c0a1ac1c6
YUSUF MIRZA OKSUZ <ymiirza@gmail.com>
2023-03-09 19:14:43 +0300

    widgets completed

--- src/components/Widgets/index.tsx
+++ src/components/Widgets/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import SearchIcon from '@mui/icons-material/Search';
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed
+} from 'react-twitter-embed';
+
 import './styles.css';
 
 export default function Widgets(){
     return <div className="widgets">
-        <h2>Widgets</h2>
+        <div className="widgets__input">
+            <SearchIcon className='widgets__search-icon' />
+            <input placeholder="Search Twitter" type="text" />
+        </div>
+
+        <div className="widgets__widget-container">
+            <h2>What's happening</h2>
+            <TwitterTweetEmbed tweetId={"1633740666634137600"} />
+            <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="pusholder"
+                options={{height: 400}}
+            />
+            <TwitterShareButton
+                url={"https://github.com/YMirzaa"}
+                options={{text: "#this dude is amazing!!", via: "mirza"}}
+            />
+        </div>
     </div>;
 }
